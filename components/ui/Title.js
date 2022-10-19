f79e9f7f57d2b9b0a93acf3c407ec9a9f655b4b3
Lucas Strand <lucas.strand@outlook.com>
2022-10-19 11:25:11 +0200

    dynamic width on title text

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -7,13 +7,15 @@ function Title({ children }) {
 export default Title;
 
 const styles = StyleSheet.create({
-    title: {
-      fontFamily: 'open-sans-bold',
-      fontSize: 24,
-      color: 'white',
-      textAlign: 'center',
-      borderWidth: 2,
-      borderColor: 'white',
-      padding: 12,
-    },
-  });
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: 'white',
+    padding: 12,
+    maxWidth: '80%',
+    width: 300,
+  },
+});
