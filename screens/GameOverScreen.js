72aee9024c1c2a07121ccdfc77db4f48b3328a44
Lucas Strand <lucas.strand@outlook.com>
2022-10-19 11:52:02 +0200

    dynamic image game over

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
@@ -23,11 +23,12 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
   );
 }
 export default GameOverScreen;
+const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   imageContainer: {
-    width: 300,
-    height: 300,
+    width: deviceWidth < 380 ? 160: 300,
+    height: deviceWidth < 380 ? 160: 300,
     borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
